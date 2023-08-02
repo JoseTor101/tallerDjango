c79b5c85367609548bb93bc9a793e47dc06c4aee
josetor101 <josetordecilla800@gmail.com>
2023-08-02 11:29:33 -0500

    Movies added, integration with Django administrator

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',movieViews.home),
     path('about',movieViews.about),
 ]
+
+#Habilitar el servidor para almacenar imágenes:
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
